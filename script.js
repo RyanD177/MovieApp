4e7569a07cfa20443e02a2582b651cac24979ba4
RyanD177 <w0477770@campus.nscc.ca>
2023-04-29 19:30:06 -0300

    fun

--- script.js
+++ script.js
@@ -1,16 +1,60 @@
-const API_KEY = "api_key=0a9a4bef069438376f2d8da0b09c8968"
-const BASE_URL ="https://api.themoviedb.org/3/";
-const API_URL = BASE_URL + "/discover/movie?sort_by=popularity.desc&" + API_KEY;
-console.log(API_URL);
+// Movie App, showing images and popularity 
+
+const APIURL = "http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+
+const imagePath = "https://image.tmdb.org/t/p/w1280";
 
 
-const getMovies = (url) => {
-    fetch(url).then(Response => Response.json()).then(moviedata => {
-        console.log(moviedata);
+async function getMovies () {
+    const response = await fetch(APIURL);
+    console.log(response);
+    const responseData =  await  response.json();
+    console.log(responseData)
+
+    responseData.results.forEach(movie => {
+        const {poster_path,title,vote_average} =  movie;
+        
+        const movieElement = document.createElement("div");
+        movieElement.classList.add("movie");
+        movieElement.innerHTML =` 
+        <div class="1"> 
+        <img src="${imagePath + poster_path}"
+        alt="${title}">
+        <h3${title}</h3>
+        <span>${vote_average}</span>
+     </div>
+     <div class="2"> 
+     <img src="${imagePath + poster_path}"
+     alt="${title}">
+     <h3${title}</h3>
+     <span>${vote_average}</span>
+  </div>
+  <div class="3"> 
+  <img src="${imagePath + poster_path}"
+  alt="${title}">
+  <h3${title}</h3>
+  <span>${vote_average}</span>
+</div>
+<div class="4"> 
+<img src="${imagePath + poster_path}"
+alt="${title}">
+<h3${title}</h3>
+<span>${vote_average}</span>
+</div>
+<div class="5"> 
+<img src="${imagePath + poster_path}"
+alt="${title}">
+<h3${title}</h3>
+<span>${vote_average}</span>
+</div>
+
+`
+document.body.appendChild(movieElement);
 
     })
 
 
 
 }
-getMovies(API_URL);
+
+getMovies();
